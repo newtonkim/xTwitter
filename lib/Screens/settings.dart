32763ba66984b12e923a1caaf7cde9556ce129ef
newtonkim <knewts98@gmail.com-iMac.local>
2025-03-27 10:42:38 +0300

    update user name on the settings screen

--- lib/Screens/settings.dart
+++ lib/Screens/settings.dart
@@ -1,14 +1,40 @@
+import 'package:demo/providers/user_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class Settings extends StatelessWidget {
+class Settings extends ConsumerStatefulWidget {
   const Settings({super.key});
 
   @override
+  ConsumerState<Settings> createState() => _SettingsState();
+}
+
+class _SettingsState extends ConsumerState<Settings> {
+  final TextEditingController _nameController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
+    LocalUser currentUser = ref.watch(userProvider);
+    _nameController.text = currentUser.user.name;
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Settings'),
-      ),
+        title: const Text('Settings')),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(labelText: "Enter Your Name"),
+                controller: _nameController,  
+              ),
+              TextButton(onPressed: () async {
+                await ref.read(userProvider.notifier).updateName(_nameController.text);
+              }, child: const Text("Update Name"))
+            ],
+          ),
+        )
+     
     );
   }
 }
